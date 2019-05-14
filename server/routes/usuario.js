6991d24bc823e44ebe6ca558ec55a8198ce8cf7b
dev <dev@MacBook-Pro-de-Carlos.local>
2019-05-14 18:12:16 +0200

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,8 +3,9 @@ const app = express();
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/authentication');
 
-app.get('/usuario', (req, res) => {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -37,7 +38,7 @@ app.get('/usuario', (req, res) => {
 
 });
 
-app.post('/usuario', (req, res) => {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let body = req.body;
 
@@ -65,7 +66,7 @@ app.post('/usuario', (req, res) => {
 
 });
 
-app.put('/usuario/:id', (req, res) => {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -116,7 +117,7 @@ app.put('/usuario/:id', (req, res) => {
 
 });*/
 
-app.delete('/usuario/:id', (req, res) => {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
 
--- server/server.js
+++ server/server.js
@@ -1,19 +1,26 @@
 require('./config/config');
 
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+
+const app = express();
+
 const bodyParser = require('body-parser');
 
+//parse aplication/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(require('./routes/usuario'))
 
-mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
+// parse aplicación json
+app.use(bodyParser.json());
+
+// Configuración global de rutas
+app.use(require('./routes/index'))
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true }, (err, res) => {
 
     if (err) throw err;
 
     console.log('Base de datos ONLINE');
 })
 
-app.listen(process.env.PORT, () => console.log(`Escuchando puerto ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`Escuchando puerto ${process.env.PORT}`));
