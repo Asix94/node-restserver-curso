89ce39137c2d063ddefad0f6249bec99ad331106
dev <dev@MacBook-Pro-de-Carlos.local>
2019-05-14 14:10:04 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://asix94:qVt466xx020GHt0n@ds155916.mlab.com:55916/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
