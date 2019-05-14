a124751707588bbf91363af39996f97d93405c25
dev <dev@MacBook-Pro-de-Carlos.local>
2019-05-14 13:41:00 +0200

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,44 +1,19 @@
+require('./config/config');
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('./config/config');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(require('./routes/usuario'))
 
-app.get('/usuario', (req, res) => {
-    res.json('get Usuario')
-});
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
 
-app.post('/usuario', (req, res) => {
+    if (err) throw err;
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        })
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-
-});
-
-app.put('/usuario/:id', (req, res) => {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete Usuario')
-});
+    console.log('Base de datos ONLINE');
+})
 
 app.listen(process.env.PORT, () => console.log(`Escuchando puerto ${process.env.PORT}`))
